5b9253d5759f9793fa0d220d57e8957d4fcf60f1
administrativetrick <48927121+administrativetrick@users.noreply.github.com>
2023-06-05 08:07:14 -0700

    Update toil_tracker.py
    Added Conversion Tool in top Menu.

--- toil_tracker.py
+++ toil_tracker.py
@@ -3,6 +3,7 @@ import sqlite3
 from datetime import timedelta
 from tkinter import *
 from tkinter import ttk, messagebox, filedialog
+import pyperclip
 
 # Connect to SQLite database (or create it if it doesn't exist)
 conn = sqlite3.connect('toil.db')
@@ -14,6 +15,7 @@ conn.execute('''
     description TEXT,
     duration INTEGER);
 ''')
+
 
 def remove_selected_items():
     selected_items = treeview.selection()  # get all selected items
@@ -34,6 +36,7 @@ def remove_selected_items():
     else:
         messagebox.showinfo("No Items Removed", "No items were removed.")
 
+
 def show_context_menu(event):
     # Select row under mouse
     treeview.identify_row(event.y)
@@ -41,6 +44,7 @@ def show_context_menu(event):
         context_menu.post(event.x_root, event.y_root)
     else:
         messagebox.showerror("Error", "No item selected!")
+
 
 def add_toil_item(description, duration):
     try:
@@ -57,9 +61,11 @@ def add_toil_item(description, duration):
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
+
 def update_toil_item(id, description, duration):
     conn.execute("UPDATE toil_items SET description = ?, duration = ? WHERE id = ?", (description, duration, id))
     conn.commit()
+
 
 def remove_toil_item(id):
     try:
@@ -73,14 +79,17 @@ def remove_toil_item(id):
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
+
 def list_toil_items():
     cursor = conn.execute("SELECT * FROM toil_items ORDER BY duration DESC")
     return cursor.fetchall()
+
 
 def get_total_toil():
     cursor = conn.execute("SELECT SUM(duration) FROM toil_items")
     total_duration = cursor.fetchone()[0]
     return total_duration if total_duration is not None else 0
+
 
 def export_to_csv():
     filename = filedialog.asksaveasfilename(defaultextension=".csv")
@@ -89,6 +98,48 @@ def export_to_csv():
         writer.writerow(["ID", "Description", "Duration"])
         writer.writerows(list_toil_items())
     messagebox.showinfo("Success", "Data exported successfully!")
+
+
+def openConversionToolWindow():
+    # Top Level Object to be treated as a new Window
+    def convert_to_seconds(years=0, months=0, days=0, hours=0, minutes=0, seconds=0):
+        seconds += minutes * 60
+        seconds += hours * 60 * 60
+        seconds += days * 24 * 60 * 60
+        seconds += months * 30 * 24 * 60 * 60  # approximating month to 30 days
+        seconds += years * 365 * 24 * 60 * 60  # approximating year to 365 days
+        return seconds
+
+    def calculate():
+        unit = unit_selector.get()
+        value = int(value_entry.get())
+        inputs = {unit.lower(): value}
+        result = convert_to_seconds(**inputs)
+        result_label.config(text=f"Total seconds: {result}")
+        return result
+
+    def copy_to_clipboard():
+        result = calculate()
+        pyperclip.copy(str(result))
+
+    conversion_win = Toplevel(root)
+    conversion_win.title('Time Convertsion Tool')
+
+    unit_selector = ttk.Combobox(conversion_win, values=['Years', 'Months', 'Days', 'Hours', 'Minutes'])
+    unit_selector.grid(column=0, row=0)
+
+    value_entry = ttk.Entry(conversion_win)
+    value_entry.grid(column=1, row=0)
+
+    calculate_button = ttk.Button(conversion_win, text="Calculate", command=calculate)
+    calculate_button.grid(column=2, row=0)
+
+    copy_button = ttk.Button(conversion_win, text="Copy to Clipboard", command=copy_to_clipboard)
+    copy_button.grid(column=3, row=0)
+
+    result_label = ttk.Label(conversion_win, text="")
+    result_label.grid(column=0, row=1, columnspan=4)
+
 
 # Define the color scheme for the dark mode
 dark_color_scheme = {
@@ -115,7 +166,7 @@ color_scheme = light_color_scheme
 
 # Initialize tkinter root window
 root = Tk()
-root.geometry("600x600")
+root.geometry("800x600")
 root.title("Toil Tracker")
 
 # Configure root window background color
@@ -125,10 +176,11 @@ root.config(bg=color_scheme["bg"])
 style = ttk.Style()
 style.theme_use("default")
 style.configure("Treeview",
-    background=color_scheme["bg"],
-    foreground=color_scheme["fg"],
-    fieldbackground=color_scheme["fieldbg"])
+                background=color_scheme["bg"],
+                foreground=color_scheme["fg"],
+                fieldbackground=color_scheme["fieldbg"])
 style.map('Treeview', background=[('selected', color_scheme["selectbg"])])
+
 
 def toggle_theme():
     global color_scheme
@@ -140,13 +192,18 @@ def toggle_theme():
     toil_duration_label.config(bg=color_scheme["bg"], fg=color_scheme["fg"])
     toil_id_label.config(bg=color_scheme["bg"], fg=color_scheme["fg"])
     toil_duration_unit_label.config(bg=color_scheme["bg"], fg=color_scheme["fg"])
-    toil_description_entry.config(bg=color_scheme["fieldbg"], fg=color_scheme["fg"], insertbackground=color_scheme["fg"])
+    toil_description_entry.config(bg=color_scheme["fieldbg"], fg=color_scheme["fg"],
+                                  insertbackground=color_scheme["fg"])
     toil_duration_entry.config(bg=color_scheme["fieldbg"], fg=color_scheme["fg"], insertbackground=color_scheme["fg"])
     toil_id_entry.config(bg=color_scheme["fieldbg"], fg=color_scheme["fg"], insertbackground=color_scheme["fg"])
-    add_button.config(bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"], activeforeground=color_scheme["selectfg"])
-    remove_button.config(bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"], activeforeground=color_scheme["selectfg"])
-    edit_button.config(bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"], activeforeground=color_scheme["selectfg"])
-    save_button.config(bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"], activeforeground=color_scheme["selectfg"])
+    add_button.config(bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"],
+                      activeforeground=color_scheme["selectfg"])
+    remove_button.config(bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"],
+                         activeforeground=color_scheme["selectfg"])
+    edit_button.config(bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"],
+                       activeforeground=color_scheme["selectfg"])
+    save_button.config(bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"],
+                       activeforeground=color_scheme["selectfg"])
     total_toil_label.config(bg=color_scheme["bg"], fg=color_scheme["fg"])
     style.configure("Treeview",
                     background=color_scheme["bg"],
@@ -170,6 +227,7 @@ def toggle_theme():
     update_treeview()
     update_total_toil()
 
+
 # Create menu bar
 menubar = Menu(root)
 
@@ -179,14 +237,19 @@ file_menu.add_command(label="Export to CSV", command=export_to_csv)
 file_menu.add_command(label="Toggle Theme", command=toggle_theme)
 file_menu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=file_menu)
+tools_menu = Menu(menubar, tearoff=0)
+tools_menu.add_command(label="Time Conversion", command=openConversionToolWindow)
+menubar.add_cascade(label="Tools", menu=tools_menu)
 
 # Apply menu bar
 root.config(menu=menubar)
+
 
 def update_treeview():
     treeview.delete(*treeview.get_children())
     for i, item in enumerate(list_toil_items()):
         treeview.insert('', 'end', values=item)
+
 
 def add_button_click():
     try:
@@ -204,6 +267,7 @@ def add_button_click():
     except ValueError:
         messagebox.showerror("Invalid input", "Duration must be a number!")
 
+
 def remove_button_click():
     try:
         remove_toil_item(int(toil_id_entry.get()))
@@ -211,6 +275,7 @@ def remove_button_click():
         update_total_toil()
     except ValueError:
         messagebox.showerror("Invalid input", "ID must be a number!")
+
 
 def edit_button_click():
     try:
@@ -224,6 +289,7 @@ def edit_button_click():
         toil_duration_entry.insert(0, int(timedelta(seconds=int(selected_item_values[2])).total_seconds()))
     except Exception as e:
         messagebox.showerror("Error", "No item selected!")
+
 
 def save_button_click():
     try:
@@ -241,6 +307,7 @@ def save_button_click():
         messagebox.showinfo("Success", "Toil updated successfully!")
     except Exception as e:
         messagebox.showerror("Error", str(e))
+
 
 def update_total_toil():
     total_toil = get_total_toil()
@@ -294,36 +361,49 @@ inputs_frame.pack(padx=15, pady=15)
 # Toil description label and entry
 toil_description_label = Label(inputs_frame, text="Toil Description:", bg=color_scheme["bg"], fg=color_scheme["fg"])
 toil_description_label.grid(row=0, column=0, sticky='e')
-toil_description_entry = Entry(inputs_frame, bg=color_scheme["fieldbg"], fg=color_scheme["fg"], insertbackground=color_scheme["fg"])
+toil_description_entry = Entry(inputs_frame, bg=color_scheme["fieldbg"], fg=color_scheme["fg"],
+                               insertbackground=color_scheme["fg"])
 toil_description_entry.grid(row=0, column=1, sticky='we')
 
 # Toil duration label and entry
 toil_duration_label = Label(inputs_frame, text="Toil Duration:", bg=color_scheme["bg"], fg=color_scheme["fg"])
 toil_duration_label.grid(row=1, column=0, sticky='e')
-toil_duration_entry = Entry(inputs_frame, bg=color_scheme["fieldbg"], fg=color_scheme["fg"], insertbackground=color_scheme["fg"])
+toil_duration_entry = Entry(inputs_frame, bg=color_scheme["fieldbg"], fg=color_scheme["fg"],
+                            insertbackground=color_scheme["fg"])
 toil_duration_entry.grid(row=1, column=1, sticky='we')
 
 # Toil duration unit label and dropdown
 toil_duration_unit_label = Label(inputs_frame, text="Duration Unit:", bg=color_scheme["bg"], fg=color_scheme["fg"])
 toil_duration_unit_label.grid(row=1, column=2, padx=(10, 0), sticky='w')
-toil_duration_unit = ttk.Combobox(inputs_frame, values=["Seconds", "Minutes", "Hours", "Days"], style="ComboboxStyle.TCombobox")
+toil_duration_unit = ttk.Combobox(inputs_frame, values=["Seconds", "Minutes", "Hours", "Days"],
+                                  style="ComboboxStyle.TCombobox")
 toil_duration_unit.set("Seconds")
 toil_duration_unit.grid(row=1, column=3, sticky='we')
 
 # Toil id label and entry
-toil_id_label = Label(inputs_frame, text="Toil ID (for editing/removing):", bg=color_scheme["bg"], fg=color_scheme["fg"])
+toil_id_label = Label(inputs_frame, text="Toil ID (for editing/removing):", bg=color_scheme["bg"],
+                      fg=color_scheme["fg"])
 toil_id_label.grid(row=2, column=0, sticky='e')
-toil_id_entry = Entry(inputs_frame, bg=color_scheme["fieldbg"], fg=color_scheme["fg"], insertbackground=color_scheme["fg"])
+toil_id_entry = Entry(inputs_frame, bg=color_scheme["fieldbg"], fg=color_scheme["fg"],
+                      insertbackground=color_scheme["fg"])
 toil_id_entry.grid(row=2, column=1, sticky='we')
 
 # Toil buttons
-add_button = Button(inputs_frame, text="Add Toil", command=add_button_click, bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"], activeforeground=color_scheme["selectfg"])
+add_button = Button(inputs_frame, text="Add Toil", command=add_button_click, bg=color_scheme["bg"],
+                    fg=color_scheme["fg"], activebackground=color_scheme["selectbg"],
+                    activeforeground=color_scheme["selectfg"])
 add_button.grid(row=3, column=1, sticky='we')
-remove_button = Button(inputs_frame, text="Remove Toil", command=remove_button_click, bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"], activeforeground=color_scheme["selectfg"])
+remove_button = Button(inputs_frame, text="Remove Toil", command=remove_button_click, bg=color_scheme["bg"],
+                       fg=color_scheme["fg"], activebackground=color_scheme["selectbg"],
+                       activeforeground=color_scheme["selectfg"])
 remove_button.grid(row=3, column=2, sticky='we')
-edit_button = Button(inputs_frame, text="Edit Toil", command=edit_button_click, bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"], activeforeground=color_scheme["selectfg"])
+edit_button = Button(inputs_frame, text="Edit Toil", command=edit_button_click, bg=color_scheme["bg"],
+                     fg=color_scheme["fg"], activebackground=color_scheme["selectbg"],
+                     activeforeground=color_scheme["selectfg"])
 edit_button.grid(row=3, column=3, sticky='we')
-save_button = Button(inputs_frame, text="Save Changes", command=save_button_click, bg=color_scheme["bg"], fg=color_scheme["fg"], activebackground=color_scheme["selectbg"], activeforeground=color_scheme["selectfg"])
+save_button = Button(inputs_frame, text="Save Changes", command=save_button_click, bg=color_scheme["bg"],
+                     fg=color_scheme["fg"], activebackground=color_scheme["selectbg"],
+                     activeforeground=color_scheme["selectfg"])
 save_button.grid(row=3, column=4, sticky='we')
 
 # Toil treeview
@@ -342,7 +422,8 @@ total_toil_label = Label(root, text="", bg=color_scheme["bg"], fg=color_scheme["
 total_toil_label.pack()
 
 # Total toil duration unit dropdown
-toil_total_duration_unit = ttk.Combobox(root, values=["Seconds", "Minutes", "Hours", "Days"], state="readonly", style="ComboboxStyle.TCombobox")
+toil_total_duration_unit = ttk.Combobox(root, values=["Seconds", "Minutes", "Hours", "Days"], state="readonly",
+                                        style="ComboboxStyle.TCombobox")
 toil_total_duration_unit.set("Seconds")
 toil_total_duration_unit.pack(pady=10)
 toil_total_duration_unit.bind("<<ComboboxSelected>>", lambda _: update_total_toil())
